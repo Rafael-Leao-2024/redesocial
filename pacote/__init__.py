2f6af882380a0af52082d842c4ccc0eb3bdcdf06
RafaelLeao <146947546+Rafael-Leao-2024@users.noreply.github.com>
2024-09-02 00:51:19 -0300

    todas

--- pacote/__init__.py
+++ pacote/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
